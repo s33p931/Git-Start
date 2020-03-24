9d5fe705fc6adaf46f8f5afd4851e33527b6e43b
s33p931 <62606212+s33p931@users.noreply.github.com>
2020-03-24 11:44:45 -0400

    Add files via upload
    Adding all of hw9

---
+++ Go Cats.R
@@ -0,0 +1,37 @@
+'1'
+
+## https://github.com/s33p931/Git-Start
+
+'2'
+
+#' Function to chant Go Cats
+#'
+#' @param Chant_in chant we are doing
+#' @return message that says "Go Cats"
+#'
+#' @export
+
+
+go_cats <- function(){
+  # sample function that returns 'go cats'
+  # RETURNS: string saying 'go cats'
+  return('go cats')
+}
+
+library(Stat408HW)
+Stat408HW::go_cats()
+
+'3'
+
+##The point of this question is to discuss the best possible way to finish the course. For the week of March 23rd, there will be a set of video lectures and this homework assignment, but no lab. I have set up 5 modules for the remaining weeks of the class. My current thought would be to have a recorded video lecture in place of our traditional Tuesday class. Students can work in groups (remotely, of course), alone, or I will work through the labs using a live video conference (likely Google Hangouts Meet or Webex).
+
+##a.
+## Do you have any suggestions about the structure of the course for the last 5 weeks?
+  ##no at this time
+
+##b.
+##What are you most concerned about with the remainder of this course?
+  ##being able to work through the code and not have the ability for face to face questions and explainations.
+##c.
+##What can I do to help you sucessfully complete this course?
+ ##Be availible for question and concerns whenever you can.
